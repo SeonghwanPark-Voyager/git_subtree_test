cd130193652840709cfcf9bd711ccb15695e5e38
ParkSeonghwan <psh@voyagergames.gg>
2025-06-23 14:39:06 +0900

    bat 파일 수정

--- subtree-pull.bat
+++ subtree-pull.bat
@@ -3,9 +3,9 @@ REM subtree-pull.bat
 REM 외부 레포의 최신 변경사항을 pull하여 현재 프로젝트에 반영
 
 REM 설정
-set REPO_URL=https://github.com/example/common-lib.git
+set REPO_URL=https://github.com/SeonghwanPark-Voyager/test_submodule.git
 set BRANCH=main
-set PREFIX=libs\common-lib
+set PREFIX=test
 
 echo [Git Subtree Pull] %REPO_URL% -> %PREFIX%
 git subtree pull --prefix=%PREFIX% %REPO_URL% %BRANCH% --squash
--- subtree-push.bat
+++ subtree-push.bat
@@ -3,9 +3,9 @@ REM subtree-push.bat
 REM 내 프로젝트의 하위 디렉토리 변경사항을 외부 레포에 push
 
 REM 설정
-set REPO_URL=https://github.com/example/common-lib.git
+set REPO_URL=https://github.com/SeonghwanPark-Voyager/test_submodule.git
 set BRANCH=main
-set PREFIX=libs\common-lib
+set PREFIX=test
 
 echo [Git Subtree Push] %PREFIX% -> %REPO_URL%
 git subtree push --prefix=%PREFIX% %REPO_URL% %BRANCH%
